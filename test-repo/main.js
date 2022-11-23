6a19aa39f9a17908373d0e3999b0ab4d6f8cce2e
Jude Omidiran <58514226+JOmidiran@users.noreply.github.com>
2022-11-23 05:48:55 +0100

    Update w test

---
+++ test-repo/main.js
@@ -0,0 +1,47 @@
+
+  // Set Dimensions
+  const xSize = 500;
+  const ySize = 500;
+  const margin = 40;
+  const xMax = xSize - margin*2;
+  const yMax = ySize - margin*2;
+  
+  // Create Random Points
+  const numPoints = 50;
+  const data = [];
+  for (let i = 0; i < numPoints; i++) {
+    data.push([Math.random() * xMax + 100, Math.random() * yMax + 100]);
+  }
+  
+  // Append SVG Object to the Page
+  const svg = d3.select("#myPlot")
+    .append("svg")
+    .append("g")
+    .attr("transform","translate(" + margin + "," + margin + ")");
+  
+  // X Axis
+  const x = d3.scaleLinear()
+    .domain([0, 600])
+    .range([0, xMax + 100]);
+  
+  svg.append("g")
+    .attr("transform", "translate(0," + yMax + ")")
+    .call(d3.axisBottom(x));
+  
+  // Y Axis
+  const y = d3.scaleLinear()
+    .domain([0, 600])
+    .range([ yMax + 100, 0]);
+  
+  svg.append("g")
+    .call(d3.axisLeft(y));
+  
+  // Dots
+  svg.append('g')
+    .selectAll("dot")
+    .data(data).enter()
+    .append("circle")
+    .attr("cx", function (d) { return d[0] } )
+    .attr("cy", function (d) { return d[1] } )
+    .attr("r", 8)
+    .style("fill", "Black");
